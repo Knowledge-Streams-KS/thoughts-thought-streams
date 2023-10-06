9b8d23a2b4a42d9f372781dc58dc4718f35c0275
Muhammad Saud <msaud5557@gmail.com>
2023-10-06 17:31:29 +0500

    Added login signup and logout functionality and created tempate for login and signup functionality

--- Thoughts/user/urls.py
+++ Thoughts/user/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     
+    
+    path('',views.HomePage,name='home'),
+    path('signup/',views.SignupPage,name='signup'),
+    path('login/',views.LoginPage,name='login'),
+    path('logout/',views.LogoutPage,name='logout'),
+    
 ]
