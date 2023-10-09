bc889ad3a2e5b15c2c81fc8b072aabe3d569df34
umairarshad <umairarshad085@gmail.com>
2023-10-09 22:42:04 +0500

    Thought create and thought list view implemented

--- Thoughts/thought/views.py
+++ Thoughts/thought/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from .models import Thought
+from django.views.generic import CreateView
+from django.views.generic.list import ListView
 
 # Create your views here.
+
+
+class ThoughtCreateView(CreateView):
+    model = Thought
+    fields = ['title','content',  'img', 'is_private']
+
+    success_url = '/thought/ThoughtListView/'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class ThoughtListView(ListView):
+    model = Thought
+    ordering = ['-created_at'] 
